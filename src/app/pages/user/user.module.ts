be162005c78bd7504e28e6a22539b7c1ec9e232a
Joseph Aje <ajejoseph22@gmail.com>
2021-02-17 07:26:06 +0100

    added newline

--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 import { SharedModule } from '../../shared/shared.module';
