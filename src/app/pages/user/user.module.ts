55d0e256aa61530732d96e58926a88f6243eb2ab
Joseph Aje <ajejoseph22@gmail.com>
2021-02-18 03:18:36 +0100

    added hover transition

--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 import { SharedModule } from '../../shared/shared.module';
