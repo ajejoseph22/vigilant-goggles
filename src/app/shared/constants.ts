ab940d61358f9e15e2af1e0ae9e91c7b44640bcd
Joseph Aje <ajejoseph22@gmail.com>
2021-02-19 21:27:43 +0100

    exported localstorage keys from a constants.ts file

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,4 @@
+export const interestsToDisplayKey = 'interests-to-display';
+export const subInterestsKey = 'sub-interests';
+export const interestPageKey = 'interest-page';
+export const interestsKey = 'interests';
